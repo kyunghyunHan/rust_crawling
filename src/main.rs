4a7be9550ba3908c76cd88f5b45a2e047e1a14b3
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-09-04 18:41:54 +0900

    feat:first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+use sui_sdk::types::base_types::SuiAddress;
+
+//tokio
+#[tokio::main]
+async fn main()->Result<(),anyhor::Error>{
+    println!("hello");
+    Ok(())
 }
+
